dcc1a2028818506216711ca6c4521c462f5bce68
wiktor <ryussexd@gmail.com>
2025-03-24 21:46:08 +0100

    added suggestion bar in input box

--- csdle/src/Pages/classic.jsx
+++ csdle/src/Pages/classic.jsx
@@ -4,7 +4,9 @@ import { Table, TableBody, TableContainer, TableCell, TableRow } from '@mui/mate
 import { Test } from './test';
 
 export function ClassicMode() {
-    const [playersAnswer, setPlayersAnswer] = useState("AK-47 | Redline");
+    const suggetions = skins.map(skin => skin.name);
+    const [focused, setFocused] = useState(false);
+    const [playersAnswer, setPlayersAnswer] = useState("");
     const selectedSkin = skins[0];
     const [showAnswer, setShowAnswer] = useState(false);
     const [playersAnswers, setPlayersAnswers] = useState([]);
@@ -26,11 +28,26 @@ export function ClassicMode() {
 
                     }}>
                     <input
-                    type="text"
-                    value={playersAnswer}
-                    onChange={(e) => setPlayersAnswer(e.target.value)}
-                    placeholder="Take a guess"
+                        type="text"
+                        value={playersAnswer}
+                        onChange={(e) => setPlayersAnswer(e.target.value)}
+                        placeholder="Take a guess"
+                        onFocus={() => setFocused(true)}
                     />
+                    {focused &&
+                    <div className='shadow-lg'>
+                        {suggetions.map((suggetion, index) => {
+                            const isMatch = suggetion.toLowerCase().indexOf(playersAnswer.toLowerCase()) > -1;
+                            return <div key={index}>
+                                {isMatch &&(
+                                    <div onClick={() => setPlayersAnswer(suggetion)}>
+                                    {suggetion}
+                                </div>
+                                )}
+                            </div>
+                            })} 
+                    </div>
+                    }
                     <button type="submit">Submit</button>
                 </form>
                 {showAnswer &&
